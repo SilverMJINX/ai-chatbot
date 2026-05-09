37e7ea2fffbcc22f06e1bfc28a41b2002a247ac6
Minx <amandatum1122@gmail.com>
2026-05-09 15:42:53 +0800

    oops fixed typo

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -256,7 +256,7 @@ function UserMenu({ name }: { name: string }) {
             </svg>
             Search Books
           </Link>
-          <Link href="/profile" className="user-dropdown-item" onClick={() => setOpen(false)}>
+          <Link href="/profile" className="dropdown-item" onClick={() => setOpen(false)}>
             <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
               <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/>
               <circle cx="12" cy="7" r="4"/>
